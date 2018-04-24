065e636a5a433919fe4da81a2d5b90064f9fe1fe
Christian Francisco <christian.francisco@wsu.edu>
2018-04-24 04:00:23 -0700

    Added dtsPrepareST() and findLoc()

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string.h>
+#include "help.cpp"
+#include "suffixtree.cpp"
+#include <vector>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	string header;
+	string sequence;
+	string alphabet;
+
+	determineArgs(argv[1], argv[2], &header, &sequence, &alphabet);
+	/* cout << header << endl;
+	cout << sequence << endl;
+	cout << alphabet << endl << endl; */
+
+	SuffixTree *suffixTree = new SuffixTree(sequence);
+	suffixTree->construction();
+	/* cout << endl << endl; */
+
+	/* suffixTree->dfsPrint();
+	cout << endl; */
+
+	/* suffixTree->dfsEnumerate();
+	cout << endl << endl; */
+
+	suffixTree->dfsBWT();
+	/* cout << endl << endl; */
+
+	/* suffixTree->dfsRepeat();
+	cout << endl; */
+
+	/* cout << sequence.length() << endl;
+	cout << suffixTree->mNumLeaves - 1 << endl; */
+
+	return 0;
+}
